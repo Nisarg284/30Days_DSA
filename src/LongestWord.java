b1b021b8d059b9a757dbbdbbc3d7d62995be833e
Nisarg <Nisargsolanki284@gmail.com>
2025-01-03 14:27:41 +0530

    Add Length of the Longest Word Program

---
+++ src/LongestWord.java
@@ -0,0 +1,29 @@
+public class LongestWord {
+
+    public static int longestLength(String str)
+    {
+        int ans=Integer.MIN_VALUE;
+        int count =0;
+
+        for(int i=0;i<str.length();i++)
+        {
+
+            if(str.charAt(i) == ' ')
+            {
+                ans = Math.max(ans,count);
+                count =0;
+            }else{
+                count++;
+            }
+
+        }
+        return ans;
+    }
+    public static void main(String[] args) {
+
+        String str = "Computer is the marvelous invention of human";
+
+        System.out.println(longestLength(str));
+
+    }
+}
